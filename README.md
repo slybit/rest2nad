4c51690e182b121dccd0f7f1b94d08342b60da9a
Stefaan Seys <stefaan.seys@onespan.com>
2020-03-18 10:12:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# REST2NAD
+
+A tiny Python based REST server to control your NAD Receiver (or other equipment) through its RS232 port.
+I guess this is only useful for older models (like mine) that do not yet have a built-in ethernet port. But I guarantee you that bying a Raspberry Pi to run this will be much cheaper than getting a new receiver :-)
+
+I run this on my Raspberry Pi based music streamer that sits next to the receiver.
+
+It allows, for example, to make some custom integrations with Home Assistant to control the receiver.
+
+## Config
+
+Just adapt the simple config.yaml file to your needs:
+```
+serial:
+  port: /dev/ttyUSB0
+rest:
+  bindIP: 0.0.0.0
+  port: 3333
+```
+
+## Using the REST interface
+
+### GET
+
+| item  | value |
+| ----- | -------- |
+| URL   | `http://host/nad/<command>` |
+
+
+Here, command is any of the valid NAD commands that can be used to read the state/value from the receiver.
+
+For example, `http://host/nad/Main.Volume` will return the current volume of the receiver.
+
+The returned value is a verbatim copy of the output from the receiver. For example, `Main.Volume=-40`.
+
+### POST (REST)
+
+| item  | value |
+| ----- | -------- |
+| URL   | `http://host/nad/<command>` |
+| BODY  | plaintext, e.g., `=-40`, `+` or `=On` |
+
+For example, `http://host/nad/Main.Volume` with body `=-40` will set the volume of the receiver to -40dB.
+
+Internally, the command from the URL and the body are simply concatenated before sending it to the receiver over RS232.
+
+Yes, it would be cleaner to have a simple JSON structure like
+```JSON
+{
+    "op" : "=",
+    "arg" : "-40"
+}
+```
+but the above also works...
+
+Again, the return value is a verbatim copy of the output from the receiver.
+
+### POST (NON-REST)
+
+| item  | value |
+| ----- | -------- |
+| URL   | `http://host/nad/` |
+| BODY  | plaintext, e.g., `Main.Volume=-40` |
+
+For example, `http://host/nad/` with body `Main.Power=On` will turn on the receiver.
+
+Internally, the command is taken verbatim from the body and sent to the receiver over RS232.
+
+Again, the return value is a verbatim copy of the output from the receiver.
---
+++ nad.py
@@ -0,0 +1,81 @@
+import yaml
+import logging
+import sys
+import serial
+from flask import Flask, request
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# ----------------------------------------------------------------------------------------------------------------------------------------
+# CONFIG
+# ----------------------------------------------------------------------------------------------------------------------------------------
+
+
+if len(sys.argv) < 2:
+    logging.critical("No config file provided.")
+    sys.exit(1)
+    
+config_file = sys.argv[1]
+
+try:
+    with open(config_file) as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+except:
+    logging.critical("[CONFIG] Could not open file config.yaml")
+    sys.exit(1) 
+  
+
+# ----------------------------------------------------------------------------------------------------------------------------------------
+# SERIAL
+# ----------------------------------------------------------------------------------------------------------------------------------------
+
+#try:
+#    nad_serial = serial.Serial(port=config['serial']['port'], baudrate=115200, xonxoff=False, rtscts=False, dsrdtr=False, timeout=0.5)
+#    logging.info("[SERIAL] Connected to " +  config['serial']['port'])
+#except:
+#    logging.critical( "[SERIAL] Could not open " + config['serial']['port'])
+#    sys.exit(1)
+
+
+# ----------------------------------------------------------------------------------------------------------------------------------------
+# REST API
+# ----------------------------------------------------------------------------------------------------------------------------------------
+app = Flask(__name__)
+
+@app.route('/nad/<command>', methods=['GET', 'POST'])
+def getCommand(command):
+    if request.method == 'POST':
+        arg = request.data
+        command = command + arg
+    else:
+        command = command + '?'
+    command = command.encode('utf-8','ignore')
+    logging.info("[SERIAL] Sending command " + command)
+    try:
+        nad_serial.write('\n' + command + '\n')
+        res = nad_serial.read_until()
+        return res
+    except serial.SerialTimeoutException:
+        return "RS232 Time out"
+    except:
+        return "RS232 Unknown error"
+
+@app.route('/nad/', methods=['POST'])
+def postCommand():
+    command = request.data
+    command = command.encode('utf-8','ignore')
+    logging.info("[SERIAL] Sending command " + command)
+    try:
+        nad_serial.write('\n' + command + '\n')
+        res = nad_serial.read_until()
+        return res
+    except serial.SerialTimeoutException:
+        return "RS232 Time out"
+    except:
+        return "RS232 Unknown error"
+
+# ----------------------------------------------------------------------------------------------------------------------------------------
+# MAIN
+# ----------------------------------------------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+    app.run(host=config['rest']['bindIP'], port=config['rest']['port'])
